063c1526d784ce51f0df7f4e1dda6b981361dbed
Aslak Hellevik <aslakhel@uio.no>
2023-06-01 15:47:38 +0200

    modified:   Regularization benchmarking/MNIST_LeNet.ipynb 	modified:   Regularization benchmarking/__pycache__/data_generators.cpython-310.pyc 	modified:   Regularization benchmarking/__pycache__/model_classes.cpython-310.pyc 	modified:   Regularization benchmarking/__pycache__/tools.cpython-310.pyc 	modified:   Regularization benchmarking/train_models.py

--- Regularization benchmarking/train_models.py
+++ Regularization benchmarking/train_models.py
@@ -1,4 +1,5 @@
 import torch
+import pickle
 from data_generators import data_loader_MNIST
 from model_classes import LeNet
 from tools import train
@@ -20,8 +21,8 @@ model_no_reg = LeNet(lr=lr, momentum=momentum, in_channels=in_channels)
 
 # Check if there are multiple GPUs, and if so, use DataParallel
 if torch.cuda.device_count() > 1:
-  print("Let's use", torch.cuda.device_count(), "GPUs!")
-  model_no_reg = DataParallel(model_no_reg)
+    print("Let's use", torch.cuda.device_count(), "GPUs!")
+    model_no_reg = DataParallel(model_no_reg)
 
 # Move the model to the device (CPU or GPU)
 model_no_reg.to(device)
@@ -32,3 +33,16 @@ losses, reg_losses, epochs, weights, train_accuracies, test_accuracies = train(
 )
 
 torch.save(model_no_reg.state_dict(), "./Trained_models/model_no_reg.pt")
+
+# Save losses, reg_losses, epochs, weights, train_accuracies, test_accuracies using pickle
+data = {
+    "losses": losses,
+    "reg_losses": reg_losses,
+    "epochs": epochs,
+    "weights": weights,
+    "train_accuracies": train_accuracies,
+    "test_accuracies": test_accuracies,
+}
+
+with open("./Trained_models/model_no_reg_data.pkl", "wb") as f:
+    pickle.dump(data, f)
