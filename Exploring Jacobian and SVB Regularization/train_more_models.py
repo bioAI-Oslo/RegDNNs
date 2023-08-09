8745071cde1462f0414e3350453143bf66737ea4
Aslak Hellevik <aslakhel@uio.no>
2023-08-09 14:35:18 +0200

    modified:   Exploring Jacobian and SVB Regularization/train_more_models.py

--- Exploring Jacobian and SVB Regularization/train_more_models.py
+++ Exploring Jacobian and SVB Regularization/train_more_models.py
@@ -10,9 +10,9 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # Set dataset
-    # dataset = "mnist"
+    dataset = "mnist"
     # dataset = "cifar10"
-    dataset = "cifar100"
+    # dataset = "cifar100"
 
     if dataset == "mnist":
         train_loader, test_loader = data_loader_MNIST()
@@ -27,30 +27,15 @@ if __name__ == "__main__":
 
     # Initialize all models and store them in a dictionary with their names
     models = {
-        # "model_no_reg_0": DDNet(),
-        # # "model_l2_0": DDNet(l2_lmbd=l2_lmbd),
-        # "model_svb_0": DDNet(svb=True),
-        # "model_jacobi_0": DDNet(jacobi=True),
-        # "model_jacobi_no_dropout_0": DDNet(dropout_rate=0.0, jacobi=True),
-        # "model_no_reg_0": DDNet(lr=0.05, dataset="cifar100"),
-        # "model_l2_0": DDNet(lr=0.05, dataset="cifar100", l2_lmbd=l2_lmbd),
-        # "model_svb_0": DDNet(lr=0.05, dataset="cifar100", svb=True),
-        # "model_jacobi_0": DDNet(lr=0.05, dataset="cifar100", jacobi=True),
-        # "model_jacobi_no_dropout_0": DDNet(
-        #     lr=0.05, dataset="cifar100", dropout_rate=0.0, jacobi=True
-        # ),
-        "model_no_reg_3": ResNet18(),
-        "model_l2_3": ResNet18(l2_lmbd=l2_lmbd),
-        "model_svb_3": ResNet18(svb=True),
-        "model_jacobi_3": ResNet18(jacobi=True),
-        "model_no_reg_4": ResNet18(),
-        "model_l2_4": ResNet18(l2_lmbd=l2_lmbd),
-        "model_svb_4": ResNet18(svb=True),
-        "model_jacobi_4": ResNet18(jacobi=True),
+        "model_no_reg_2": LeNet_MNIST(),
+        "model_l2_2": LeNet_MNIST(l2_lmbd=0.0005),
+        "model_svb_2": LeNet_MNIST(svb=True),
+        "model_jacobi_2": LeNet_MNIST(jacobi=True),
+        "model_jacobi_no_dropout_2": LeNet_MNIST(dropout_rate=0.0, jacobi=True),
     }
 
     # Number of epochs for training, 250 in Hoffman 2019
-    n_epochs = 50
+    n_epochs = 250
 
     # Iterate through each model
     for model_name, model in models.items():
